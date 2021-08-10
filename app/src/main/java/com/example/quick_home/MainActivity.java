8202b47600d83aeb63b80519c68df69d0b6a8154
bennVik <v.benini99@gmail.com>
2021-08-10 15:59:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/quick_home/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.quick_home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText home_text = (EditText) findViewById(R.id.Home_Text);     // define Button
+        final Button search_button = findViewById(R.id.Search_Button);          // define Search_text
+
+        search_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // execute after button pressed
+                String home = home_text.getText().toString();       // get text from home_text
+
+                if(home != ""){
+                    home_text.setText("Search...");
+                }
+
+            }
+        });
+    }
+}
