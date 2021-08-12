074034c74727b0fd0541a9d707fcecbb03e5d202
bennVik <v.benini99@gmail.com>
2021-08-13 01:49:46 +0200

    Changed View - small changes on location

--- app/src/main/java/com/example/quick_home/MainActivity.java
+++ app/src/main/java/com/example/quick_home/MainActivity.java
@@ -16,9 +16,11 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -36,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     // Assign variables     (Global)
     EditText home_text;     // define Button
     Button search_button;          // define Search_text
+    TextView text_view1;
+    TextView text_view2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +48,10 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize variables
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        home_text =  (EditText) findViewById(R.id.Home_Text);
+        home_text = (EditText) findViewById(R.id.Home_Text);
         search_button = findViewById(R.id.Search_Button);
+        text_view1 = findViewById(R.id.textView);
+        text_view2 = findViewById(R.id.textView2);
 
         // Location     OLD !!!
         //--------------------------------------------------------------------------------------------------
@@ -58,25 +64,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 // Check Location permission
-                //-----------------------------------------------------------------------------------------
-                if(ActivityCompat.checkSelfPermission(MainActivity.this
-                        , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+/*                //-----------------------------------------------------------------------------------------
+                if (ActivityCompat.checkSelfPermission(MainActivity.this
+                        , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     // When permission granted
                     getLocation();
-                }else{
+                } else {
                     // When permission denied
                     ActivityCompat.requestPermissions(MainActivity.this
-                    , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                            , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+
                 }
+  */
+                getLocation();
                 //-----------------------------------------------------------------------------------------
-
-
                 // execute after button pressed
                 String home = home_text.getText().toString();       // get text from home_text
 
-                if(home != "Hallo"){
+                if (home != "Hallo") {
                     home_text.setText("Search...");
-                }else{
+                } else {
                     home_text.setText("Not Found");
                 }
             }
@@ -84,14 +91,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @SuppressLint("MissingPermission")  // ?!   --------------------------------------------------------------
     private void getLocation() {
+        // If abfrage hier da Java sich beschwert
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this
+                    , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+
+            return;
+        }
         fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
             @Override
             public void onComplete(@NonNull Task<Location> task) {
                 // Initialize location
                 Location location = task.getResult();
-                if(location != null){
+                if (location != null) {                         // Location ist Null, function wird übersprungen !?
+
                     try {
                         //Initialize geoCoder
                         Geocoder geocoder = new Geocoder(MainActivity.this,
@@ -101,9 +115,22 @@ public class MainActivity extends AppCompatActivity {
                                 location.getLatitude(), location.getLongitude(), 1
                         );
                         // Set Latitude on TextView
+                        text_view1.setText(Html.fromHtml(                                           // ausgabe der coordinaten brete
+                                "<font color?'#6200'><b>Latitude :</b><br></font>"
+                                        + address.get(0).getLatitude()
+                        ));
+                        // Set Longitude
+                        text_view2.setText(Html.fromHtml(                                           // ausgabe der coordinaten länge
+                                "<font color?'#6200'><b>Longitude :</b><br></font>"
+                                        + address.get(0).getLongitude()
+                        ));
+
+                        text_view1.setText("Doooooo");          // test ob function aufgerufen wird
                     } catch (IOException e) {
                         e.printStackTrace();
+                        text_view1.setText("Faile");            // test
                     }
+
                 }
             }
         });
